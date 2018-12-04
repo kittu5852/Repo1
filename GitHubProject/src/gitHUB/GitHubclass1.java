b0794451b036f47e05ae861835b486be9b87f50b
kittu5852 <45411081+kittu5852@users.noreply.github.com>
2018-12-04 09:59:23 +0530

    Update GitHubclass1.java

--- GitHubProject/src/gitHUB/GitHubclass1.java
+++ GitHubProject/src/gitHUB/GitHubclass1.java
@@ -6,6 +6,7 @@ public class GitHubclass1 {
 		// TODO Auto-generated method stub
 		System.out.println("GitHub class one : print Hello World");
 		System.out.println("GitHub Class one: Added new line of code changes");
+		System.out.println("I have added code in Git Repository to push it to eclipse");
 	}
 
 }
