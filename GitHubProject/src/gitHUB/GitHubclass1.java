e6276439712e81d0bbee974fb7622358ad2b681c
Krishna <chinnikrishnaonline@gmail.com>
2018-12-04 09:48:14 +0530

    second commit

---
+++ GitHubProject/src/gitHUB/GitHubclass1.java
@@ -0,0 +1,10 @@
+package gitHUB;
+
+public class GitHubclass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("GitHub class one : print Hello World");
+	}
+
+}
