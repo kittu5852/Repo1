f15d5e30661e3d99be49e014c673202a502a80c4
Krishna <chinnikrishnaonline@gmail.com>
2018-12-04 09:55:59 +0530

    thirdCommit

--- GitHubProject/src/gitHUB/GitHubclass1.java
+++ GitHubProject/src/gitHUB/GitHubclass1.java
@@ -5,6 +5,7 @@ public class GitHubclass1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("GitHub class one : print Hello World");
+		System.out.println("GitHub Class one: Added new line of code changes");
 	}
 
 }
